ec89dfc630ee9967bae82b2faf6d7e919aee23ab
nharalam32 <nharalam32@gmail.com>
2024-04-22 15:29:56 -0400

    added flask stuff

--- index.py
+++ index.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('base.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
