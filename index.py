4a5ddca28e900dd789dbf814757eba108aa65ed8
chrisG759 <150283599+chrisG759@users.noreply.github.com>
2024-04-24 12:42:37 -0400

    test create

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
-from sqlalchemy import table, text
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
