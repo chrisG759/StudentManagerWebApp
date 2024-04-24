8b65e9d54222b42bfbaed7253f2a5a22f775638b
nharalam32 <nharalam32@gmail.com>
2024-04-24 13:39:24 -0400

    changes

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sqlalchemy import table, text
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Pennsylvania2004!@localhost/fp160'
 db = SQLAlchemy(app)
 
 @app.route('/')
@@ -13,17 +14,23 @@ def index():
 def register():
     return render_template('register.html')
 
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['Name']
+    account_type = request.form['account_type']  # Get the selected account type
+
+    # Create a new User object with the provided data
+    new_user = User(name=name, account_type=account_type)
+
+    # Add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return 'User registered successfully!'
+
 @app.route('/login')
 def login ():
     return render_template('login.html')
-
-@app.route('/student_test')
-def student_test():
-    return render_template('student_test.html')
-
-@app.route('/test_create')
-def create_test():
-    return render_template('test_create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
