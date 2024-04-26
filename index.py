9108e2e5b346cfab3846cc6ac81dd3bcccf17aee
chrisG759 <150283599+chrisG759@users.noreply.github.com>
2024-04-26 15:41:58 -0400

    hello

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from secrets import token_hex
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,7 +9,7 @@ app.secret_key = token_hex()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://chris:sirhc@172.16.181.82/fp160'
 
 engine = create_engine('mysql://chris:sirhc@172.16.181.82/fp160')
-sql = engine.connect()
+Session = sessionmaker(bind=engine)
 
 db = SQLAlchemy(app)
 
@@ -18,6 +18,21 @@ class Student(db.Model):
 
     account_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
+
+    # Adjust the Student model to automatically assign IDs starting from 100
+    @staticmethod
+    def generate_next_id():
+        session = Session()
+        last_student = session.query(Student).order_by(Student.account_id.desc()).first()
+        session.close()
+        if last_student:
+            return last_student.account_id + 1
+        else:
+            return 100
+
+    def __init__(self, name):
+        self.name = name
+        self.account_id = Student.generate_next_id()
 
 class Teacher(db.Model):
     __tablename__ = 'teachers'
@@ -72,11 +87,11 @@ def create_test():
             db.session.add(test_question)
         db.session.commit()
 
-        return redirect(url_for('index'))
+        return redirect(url_for('student_test'))
+ 
     else:
         questions = Question.query.all()
         return render_template('test_create.html', questions=questions)
-
 
 @app.route('/register')
 def register():
@@ -117,25 +132,28 @@ def handle_login():
         except ValueError:
             return render_template('login.html', error='Invalid account number')
         
-        # Check if the account number belongs to a student
-        if 1 <= account_number <= 9:
-            user = Student.query.filter_by(account_id=account_number).first()
         # Check if the account number belongs to a teacher
-        elif 101 <= account_number <= 109:
+        if account_number >= 100:
+            print("Teacher login attempted")
             user = Teacher.query.filter_by(account_id=account_number).first()
         else:
-            return render_template('login.html', error='Invalid account number')
+            print("Student login attempted")
+            user = Student.query.filter_by(account_id=account_number).first()
         
         if user:
+            print("User found:", user.name)
             session['user_id'] = user.account_id
             if isinstance(user, Student):
                 return redirect(url_for('student_test'))
             else:
-                return redirect(url_for('create_test'))
+                return redirect(url_for('test_create'))
         else:
             return render_template('login.html', error='User not found')
     else:
         return render_template('login.html')
+
+
+
 
 @app.route('/')
 def home():
@@ -150,13 +168,36 @@ def home():
 
 @app.route('/student_test')
 def student_test():
-    return render_template('student_test.html')
+    # Query the database for tests created by teachers
+    tests = Test.query.filter(Test.teacher_id != None).all()
+    return render_template('student_test.html', tests=tests)
+
+def add_questions():
+    question1 = Question(question='What is 1+1?', answer=2)
+    question2 = Question(question='What is 1+2?', answer=3)
+    question3 = Question(question='What is 1+3?', answer=4)
+    question4 = Question(question='What is 1+4?', answer=5)
+    question5 = Question(question='What is 1+5?', answer=6)
+    question6 = Question(question='What is 1+6?', answer=7)
+    question7 = Question(question='What is 1+7?', answer=8)
+    question8 = Question(question='What is 1+8?', answer=9)
+    question9 = Question(question='What is 1+9?', answer=10)
+    question10 = Question(question='What is 1+10?', answer=11)
+
+    with app.app_context():
+        db.metadata.reflect(engine)
+        db.metadata.drop_all(engine)
+        db.metadata.create_all(engine)
+
+        db.session.add_all([question1, question2, question3, question4, question5,
+                            question6, question7, question8, question9, question10])
+        db.session.commit()
+
 
 if __name__ == '__main__':
     db.metadata.reflect(engine)
     db.metadata.drop_all(engine)
     db.metadata.create_all(engine)
-
-    quetsion1 = Question()
+    add_questions()
 
     app.run(debug=True)
